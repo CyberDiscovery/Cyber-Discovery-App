97b24361e00db2510e2499f4fe15bcc2aedeaa1b
REDACTED-REDACTED <31963635+Dan-McL@users.noreply.github.com>
2018-06-19 16:26:33 +0100

    fixed render errors due to embeded tweets

--- lib/widgets/blog_card.dart
+++ lib/widgets/blog_card.dart
@@ -1,25 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:cyber_discovery/blog_data.dart';
 
-import 'package:flutter_html_view/flutter_html_view.dart';
+import 'package:html2md/html2md.dart' as html2md;
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 class BlogCard extends StatelessWidget {
   final BlogData _blogData;
-  
+
   BlogCard(this._blogData);
 
   @override
   Widget build(BuildContext context) {
 
-    List<Widget> categories = [];
-    _blogData.categories.forEach(
-      (String category){
-        categories.add(new Card(
-          child:  new Text(category)
-          )
-        );
+    int start = 0;
+    int end = 0;
+    String data = _blogData.content;
+    while (true) {
+      if((start = data.indexOf("<scr")) != -1){
+        end = data.indexOf("</scr", start) + 9;
+        data = data.replaceRange(start, end, "");
+      }else {
+        break;
       }
-    );
+    }
+
+    while (true) {
+      if((start = data.indexOf("<sty")) != -1){
+        end = data.indexOf("</sty", start) + 8;
+        data = data.replaceRange(start, end, "");
+      }else {
+        break;
+      }
+    }
 
     return Padding(
       padding: new EdgeInsets.symmetric(vertical: 15.0, horizontal: 5.0),
@@ -28,14 +40,16 @@ class BlogCard extends StatelessWidget {
         child: new Column(
           children: <Widget>[
             new Padding(
-              padding: new EdgeInsets.only(top: 15.0),
-              child: new Text(_blogData.title, style: Theme.of(context).textTheme.title),
+              padding: new EdgeInsets.only(top: 16.0, bottom: 8.0),
+              child: new Text(_blogData.title,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.title
+              ),
             ),
-            new HtmlView(data: _blogData.content),
-            new Flex(
-              direction: Axis.horizontal,
-              verticalDirection: VerticalDirection.down,
-              children: categories,
+            new Divider(),
+            new Padding(
+              padding: new EdgeInsets.all(5.0),
+              child: new MarkdownBody(data: html2md.convert(data)),
             )
           ],
         ),
