bd656ae8c4173b9ee35032686f8b2b2f3d31fc91
REDACTED-REDACTED <31963635+Dan-McL@users.noreply.github.com>
2018-06-22 15:36:36 +0100

    Setup schedule tabs

---
+++ lib/widgets/schedule_tab.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ScheduleTab extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: new Text("Tab"),
+    );
+  }
+}
