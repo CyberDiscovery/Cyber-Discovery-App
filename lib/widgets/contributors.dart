9026d6b1e13f8554b786de041d271addf888be67
REDACTED-REDACTED <31963635+Dan-McL@users.noreply.github.com>
2018-10-04 15:28:10 +0100

    Removed repeated code

--- lib/widgets/contributors.dart
+++ lib/widgets/contributors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:url_launcher/url_launcher.dart';
+import 'package:cyber_discovery/widgets/link.dart';
 
 class Contributors extends StatelessWidget {
   @override
@@ -23,51 +23,11 @@ class Contributors extends StatelessWidget {
                         padding: new EdgeInsets.symmetric(vertical: 5.0),
                         child: new Text("This app is the result of some amazing people from the community! Why not give them a follow and show your appreciation?"),
                       ),
-                      new GestureDetector(
-                        child: new Padding(
-                          padding: new EdgeInsets.symmetric(vertical: 5.0),
-                          child: new Text("@BenTechy66 - Building the IOS version without paying a cent", style: new TextStyle(color: Colors.lightBlue)),
-                        ),
-                        onTap: (){
-                          launch("https://twitter.com/BenTechy66");
-                        },
-                      ),
-                      new GestureDetector(
-                        child: new Padding(
-                          padding: new EdgeInsets.symmetric(vertical: 5.0),
-                          child: new Text("@thebeanogamer - Financial support and marketing", style: new TextStyle(color: Colors.lightBlue)),
-                        ),
-                        onTap: (){
-                          launch("https://twitter.com/thebeanogamer");
-                        },
-                      ),
-                      new GestureDetector(
-                        child: new Padding(
-                          padding: new EdgeInsets.symmetric(vertical: 5.0),
-                          child: new Text("@dwouca - Curing my dislexia and the website", style: new TextStyle(color: Colors.lightBlue)),
-                        ),
-                        onTap: (){
-                          launch("https://twitter.com/dwouca");
-                        },
-                      ),
-                      new GestureDetector(
-                        child: new Padding(
-                          padding: new EdgeInsets.symmetric(vertical: 5.0),
-                          child: new Text("@OwlNamedSeymour - Everything else", style: new TextStyle(color: Colors.lightBlue)),
-                        ),
-                        onTap: (){
-                          launch("https://twitter.com/OwlNamedSeymour");
-                        },
-                      ),
-                      new GestureDetector(
-                        child: new Padding(
-                          padding: new EdgeInsets.symmetric(vertical: 5.0),
-                          child: new Text("@BritMonk3y - Graphic Design", style: new TextStyle(color: Colors.lightBlue)),
-                        ),
-                        onTap: (){
-                          launch("https://twitter.com/BritMonk3y");
-                        },
-                      ),
+                      new Link("@BenTechy66 - Building the IOS version without paying a cent", "https://twitter.com/BenTechy66"),
+                      new Link("@thebeanogamer - Financial support and marketing", "https://twitter.com/thebeanogamer"),
+                      new Link("@dwouca - Curing my dislexia and the website", "https://twitter.com/dwouca"),
+                      new Link("@OwlNamedSeymour - Everything else", "https://twitter.com/OwlNamedSeymour"),
+                      new Link("@BritMonk3y - Graphic Design", "https://twitter.com/BritMonk3y"),
                     ],
                   ),
                 )
