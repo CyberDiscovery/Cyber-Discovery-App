6b1dc51c49e9675816131e4f3d0c3b666ff35683
REDACTED <31963635+REDACTED-REDACTED@users.noreply.github.com>
2018-10-04 15:45:46 +0100

    fixes #27 again (#32)
    * Updated image and removed redundant code
    
    * Added padding around text
    
    * Removed repeated code
    
    * Increased Padding

--- lib/widgets/contributors.dart
+++ lib/widgets/contributors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:url_launcher/url_launcher.dart';
+import 'package:cyber_discovery/widgets/link.dart';
 
 class Contributors extends StatelessWidget {
   @override
@@ -8,63 +8,32 @@ class Contributors extends StatelessWidget {
     return new Padding(
       padding: new EdgeInsets.symmetric(vertical: 10.0),
       child: new Card(
-        child: new Padding(
-          padding: new EdgeInsets.all(5.0),
-          child: new Column(
-            children: <Widget>[
-              new Image.asset("assets/images/contributors.png"),
-              new Text("Contributors", style: Theme.of(context).textTheme.headline),
-              new Padding(
-                padding: new EdgeInsets.symmetric(vertical: 5.0),
-                child: new Text("This app is the result of some amazing people from the community! Why not give them a follow and show your appreciation?"),
-              ),
-              new GestureDetector(
-                child: new Padding(
-                  padding: new EdgeInsets.symmetric(vertical: 5.0),
-                  child: new Text("@BenTechy66 - Building the IOS version without paying a cent", style: new TextStyle(color: Colors.lightBlue)),
-                ),
-                onTap: (){
-                  launch("https://twitter.com/BenTechy66");
-                },
-              ),
-              new GestureDetector(
-                child: new Padding(
-                  padding: new EdgeInsets.symmetric(vertical: 5.0),
-                  child: new Text("@thebeanogamer - Financial support and marketing", style: new TextStyle(color: Colors.lightBlue)),
-                ),
-                onTap: (){
-                  launch("https://twitter.com/thebeanogamer");
-                },
-              ),
-              new GestureDetector(
-                child: new Padding(
-                  padding: new EdgeInsets.symmetric(vertical: 5.0),
-                  child: new Text("@dwouca - Curing my dislexia and the website", style: new TextStyle(color: Colors.lightBlue)),
-                ),
-                onTap: (){
-                  launch("https://twitter.com/dwouca");
-                },
-              ),
-              new GestureDetector(
-                child: new Padding(
-                  padding: new EdgeInsets.symmetric(vertical: 5.0),
-                  child: new Text("@OwlNamedSeymour - Everything else", style: new TextStyle(color: Colors.lightBlue)),
-                ),
-                onTap: (){
-                  launch("https://twitter.com/OwlNamedSeymour");
-                },
-              ),
-              new GestureDetector(
-                child: new Padding(
-                  padding: new EdgeInsets.symmetric(vertical: 5.0),
-                  child: new Text("@BritMonk3y - Graphic Design", style: new TextStyle(color: Colors.lightBlue)),
-                ),
-                onTap: (){
-                  launch("https://twitter.com/BritMonk3y");
-                },
-              ),
-            ],
-          ),
+        margin: new EdgeInsets.all(0.0),
+        child: new Column(
+          children: <Widget>[
+            new Column(
+              children: <Widget>[
+                new Image.asset("assets/images/contributors.png"),
+                new Padding(
+                  padding: new EdgeInsets.all(15.0),
+                  child: new Column(
+                    children: <Widget>[
+                      new Text("Contributors", style: Theme.of(context).textTheme.headline),
+                      new Padding(
+                        padding: new EdgeInsets.symmetric(vertical: 5.0),
+                        child: new Text("This app is the result of some amazing people from the community! Why not give them a follow and show your appreciation?"),
+                      ),
+                      new Link("@BenTechy66 - Building the IOS version without paying a cent", "https://twitter.com/BenTechy66"),
+                      new Link("@thebeanogamer - Financial support and marketing", "https://twitter.com/thebeanogamer"),
+                      new Link("@dwouca - Curing my dislexia and the website", "https://twitter.com/dwouca"),
+                      new Link("@OwlNamedSeymour - Everything else", "https://twitter.com/OwlNamedSeymour"),
+                      new Link("@BritMonk3y - Graphic Design", "https://twitter.com/BritMonk3y"),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
         ),
       )
     );
