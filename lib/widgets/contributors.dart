b247e1cba118f9d96e8d9a66a9e1a9e7b7ed6e34
REDACTED-REDACTED <31963635+Dan-McL@users.noreply.github.com>
2018-10-04 15:21:38 +0100

    Added padding around text

--- lib/widgets/contributors.dart
+++ lib/widgets/contributors.dart
@@ -8,63 +8,72 @@ class Contributors extends StatelessWidget {
     return new Padding(
       padding: new EdgeInsets.symmetric(vertical: 10.0),
       child: new Card(
-        child: new Padding(
-          padding: new EdgeInsets.all(5.0),
-          child: new Column(
-            children: <Widget>[
-              new Image.asset("assets/images/contributors.png"),
-              new Text("Contributors", style: Theme.of(context).textTheme.headline),
-              new Padding(
-                padding: new EdgeInsets.symmetric(vertical: 5.0),
-                child: new Text("This app is the result of some amazing people from the community! Why not give them a follow and show your appreciation?"),
-              ),
-              new GestureDetector(
-                child: new Padding(
-                  padding: new EdgeInsets.symmetric(vertical: 5.0),
-                  child: new Text("@BenTechy66 - Building the IOS version without paying a cent", style: new TextStyle(color: Colors.lightBlue)),
-                ),
-                onTap: (){
-                  launch("https://twitter.com/BenTechy66");
-                },
-              ),
-              new GestureDetector(
-                child: new Padding(
-                  padding: new EdgeInsets.symmetric(vertical: 5.0),
-                  child: new Text("@thebeanogamer - Financial support and marketing", style: new TextStyle(color: Colors.lightBlue)),
-                ),
-                onTap: (){
-                  launch("https://twitter.com/thebeanogamer");
-                },
-              ),
-              new GestureDetector(
-                child: new Padding(
-                  padding: new EdgeInsets.symmetric(vertical: 5.0),
-                  child: new Text("@dwouca - Curing my dislexia and the website", style: new TextStyle(color: Colors.lightBlue)),
-                ),
-                onTap: (){
-                  launch("https://twitter.com/dwouca");
-                },
-              ),
-              new GestureDetector(
-                child: new Padding(
-                  padding: new EdgeInsets.symmetric(vertical: 5.0),
-                  child: new Text("@OwlNamedSeymour - Everything else", style: new TextStyle(color: Colors.lightBlue)),
-                ),
-                onTap: (){
-                  launch("https://twitter.com/OwlNamedSeymour");
-                },
-              ),
-              new GestureDetector(
-                child: new Padding(
-                  padding: new EdgeInsets.symmetric(vertical: 5.0),
-                  child: new Text("@BritMonk3y - Graphic Design", style: new TextStyle(color: Colors.lightBlue)),
-                ),
-                onTap: (){
-                  launch("https://twitter.com/BritMonk3y");
-                },
-              ),
-            ],
-          ),
+        margin: new EdgeInsets.all(0.0),
+        child: new Column(
+          children: <Widget>[
+            new Column(
+              children: <Widget>[
+                new Image.asset("assets/images/contributors.png"),
+                new Padding(
+                  padding: new EdgeInsets.all(5.0),
+                  child: new Column(
+                    children: <Widget>[
+                      new Text("Contributors", style: Theme.of(context).textTheme.headline),
+                      new Padding(
+                        padding: new EdgeInsets.symmetric(vertical: 5.0),
+                        child: new Text("This app is the result of some amazing people from the community! Why not give them a follow and show your appreciation?"),
+                      ),
+                      new GestureDetector(
+                        child: new Padding(
+                          padding: new EdgeInsets.symmetric(vertical: 5.0),
+                          child: new Text("@BenTechy66 - Building the IOS version without paying a cent", style: new TextStyle(color: Colors.lightBlue)),
+                        ),
+                        onTap: (){
+                          launch("https://twitter.com/BenTechy66");
+                        },
+                      ),
+                      new GestureDetector(
+                        child: new Padding(
+                          padding: new EdgeInsets.symmetric(vertical: 5.0),
+                          child: new Text("@thebeanogamer - Financial support and marketing", style: new TextStyle(color: Colors.lightBlue)),
+                        ),
+                        onTap: (){
+                          launch("https://twitter.com/thebeanogamer");
+                        },
+                      ),
+                      new GestureDetector(
+                        child: new Padding(
+                          padding: new EdgeInsets.symmetric(vertical: 5.0),
+                          child: new Text("@dwouca - Curing my dislexia and the website", style: new TextStyle(color: Colors.lightBlue)),
+                        ),
+                        onTap: (){
+                          launch("https://twitter.com/dwouca");
+                        },
+                      ),
+                      new GestureDetector(
+                        child: new Padding(
+                          padding: new EdgeInsets.symmetric(vertical: 5.0),
+                          child: new Text("@OwlNamedSeymour - Everything else", style: new TextStyle(color: Colors.lightBlue)),
+                        ),
+                        onTap: (){
+                          launch("https://twitter.com/OwlNamedSeymour");
+                        },
+                      ),
+                      new GestureDetector(
+                        child: new Padding(
+                          padding: new EdgeInsets.symmetric(vertical: 5.0),
+                          child: new Text("@BritMonk3y - Graphic Design", style: new TextStyle(color: Colors.lightBlue)),
+                        ),
+                        onTap: (){
+                          launch("https://twitter.com/BritMonk3y");
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
         ),
       )
     );
